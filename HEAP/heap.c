832ee4109b6a3e0b741bd69c55f70f0cb6a95bf5
jeremias98 <ramirezjeremias98@gmail.com>
2018-10-30 20:28:51 -0300

    Algunas primitivas del heap

--- HEAP/heap.c
+++ HEAP/heap.c
@@ -1,21 +1,67 @@
 #include "heap.h"
 #include <stdlib.h>
 
+#define CAPACIDAD_INICIAL 10
+#define FACTOR_AGRANDAR 2
+#define FACTOR_ACHICAR 0.5
+
 typedef struct heap {
     size_t tam;
+    size_t capacidad;
     void** arr;
     cmp_func_t cmp;
 } heap_t;
+
+bool heap_redimensionar(heap_t* heap, const double factor) {
+
+    size_t capacidad_nueva = (size_t)((double)heap->capacidad * factor);
+
+	heap->arr = realloc(heap->arr, capacidad_nueva * sizeof(void*));
+
+	if (!heap->arr) {
+		heap_destruir(heap, NULL);
+		return false;
+	}
+
+	heap->capacidad = capacidad_nueva;
+
+	return true;
+
+}
 
 heap_t *heap_crear(cmp_func_t cmp) {
 
     heap_t* heap = malloc(sizeof(heap_t));
     if (!heap) return NULL;
 
-    heap->arr = NULL;
+    heap->arr = malloc(CAPACIDAD_INICIAL * sizeof(void*));
     heap->tam = 0;
+    heap->capacidad = CAPACIDAD_INICIAL;
     heap->cmp = cmp;
 
     return heap;
+
+}
+
+void heap_destruir(heap_t *heap, void destruir_elemento(void *e)) {
+
+    size_t index = 0;
+
+    void* dato = heap->tam > 0 ? heap->arr[index] : NULL;
+
+    while(dato) {
+
+        if (destruir_elemento) {
+            destruir_elemento(dato);
+        }
+
+        index++;
+
+        dato = heap->arr[index];
+
+    }
+
+    free(heap->arr);
+    free(heap);
 
 }
--- HEAP/pruebas_alumno.c
+++ HEAP/pruebas_alumno.c
@@ -2,10 +2,15 @@
 #include "heap.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 
 void test_insertar() {
 	printf("\n####### TEST INSERTAR #######\n");
+
+	heap_t* heap = heap_crear(NULL);
+
+	heap_destruir(heap, NULL);
 }
 
 void pruebas_heap_alumno(void) {
